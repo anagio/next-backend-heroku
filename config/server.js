1da279e5ab74631d0cf39be4e595c932f0b1a69a
Peter <anagiop@gmail.com>
2022-08-14 23:37:39 -0400

    first commit

---
+++ config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', '6c71aa918241ee6d7f513caf5a39a46c'),
+    },
+  },
+});
---
+++ extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || '8df8546d-8b2b-44ec-b4ea-e36ab8ebd9fa'
+};
